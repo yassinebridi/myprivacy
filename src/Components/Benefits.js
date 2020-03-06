a2316aa768a5d0719efbbbf6f45898abcfd0f877
Yassine Bridi <yassinebridii@gmail.com>
2020-03-06 22:43:02 +0100

    Refactor Benefits to be stack componetized

--- src/Components/Benefits.js
+++ src/Components/Benefits.js
@@ -1,5 +1,32 @@
 import React from "react";
-import { Box, Heading, Flex, Text, Button, Image } from "@chakra-ui/core";
+import {
+  Box,
+  Heading,
+  Flex,
+  Text,
+  Button,
+  Image,
+  Stack
+} from "@chakra-ui/core";
+
+const Feature = ({ image, title, desc }) => {
+  return (
+    <Box flexGrow={1} flexBasis={0} mx={3}>
+      <Image
+        src={image}
+        alt="lock"
+        size="120px"
+        margin="0 auto 10px auto"
+      ></Image>
+      <Text fontSize="xl" fontWeight="bold" pb="2">
+        {title}
+      </Text>
+      <Text fontWeight="light" fontSize="lg">
+        {desc}
+      </Text>
+    </Box>
+  );
+};
 
 const Benefits = () => {
   return (
@@ -19,68 +46,33 @@ const Benefits = () => {
         We use the power of Artifitial intelligence <br></br>to make our
         customers's life easier and safer.
       </Text>
-      <Flex py={10} align="start">
-        <Box flexGrow={1} flexBasis={0} mx={3}>
-          <Image
-            src="https://res.cloudinary.com/gokoura/image/upload/v1582937079/25496_Converted_hprndj.svg"
-            alt="lock"
-            size="120px"
-            margin="0 auto 10px auto"
-          ></Image>
-          <Text fontSize="xl" fontWeight="bold" pb="2">
-            Secure
-          </Text>
-          <Text fontWeight="light" fontSize="lg">
-            Soley is very secure as there is simply no access to the lock –
-            unlike mechanical locks which can be lock picked.
-          </Text>
-        </Box>
-        <Box flexGrow={1} flexBasis={0} mx={3}>
-          <Image
-            src="https://res.cloudinary.com/gokoura/image/upload/v1582937960/circuit_vzywb9.svg"
-            alt="lock"
-            size="120px"
-            margin="0 auto 10px auto"
-          ></Image>
-          <Text fontSize="xl" fontWeight="bold" pb="2">
-            Reliable
-          </Text>
-          <Text fontWeight="light" fontSize="lg">
-            Electronic mechanisms are far more durable than physical ones.
-            There’s very little which can be broken or worn out.
-          </Text>
-        </Box>
-        <Box flexGrow={1} flexBasis={0} mx={3}>
-          <Image
-            src="https://res.cloudinary.com/gokoura/image/upload/v1582937682/time_uqltwb.svg"
-            alt="lock"
-            size="120px"
-            margin="0 auto 10px auto"
-          ></Image>
-          <Text fontSize="xl" fontWeight="bold" pb="2">
-            Easy Access
-          </Text>
-          <Text fontWeight="light" fontSize="lg">
-            Soley uses Face Detection techniques to unlock your safe, it takes
-            almost not time.
-          </Text>
-        </Box>
-        <Box flexGrow={1} flexBasis={0} mx={3}>
-          <Image
-            src="https://res.cloudinary.com/gokoura/image/upload/v1582938129/key_pkmkfx.svg"
-            alt="lock"
-            size="120px"
-            margin="0 auto 10px auto"
-          ></Image>
-          <Text fontSize="xl" fontWeight="bold" pb="2">
-            Convenience
-          </Text>
-          <Text fontWeight="light" fontSize="lg">
-            You never have to worry about carrying around or losing your locking
-            key again.
-          </Text>
-        </Box>
-      </Flex>
+
+      <Stack isInline spacing={8} py={10} align="start">
+        <Feature
+          title="Secure"
+          desc="Soley is very secure as there is simply no access to the lock –
+            unlike mechanical locks which can be lock picked."
+          image="https://res.cloudinary.com/gokoura/image/upload/v1582937079/25496_Converted_hprndj.svg"
+        />
+        <Feature
+          title="Reliable"
+          desc="Electronic mechanisms are far more durable than physical ones.
+            There’s very little which can be broken or worn out."
+          image="https://res.cloudinary.com/gokoura/image/upload/v1582937960/circuit_vzywb9.svg"
+        />
+        <Feature
+          title="Easy Access"
+          desc="Soley uses Face Detection techniques to unlock your safe, it takes
+            almost not time."
+          image="https://res.cloudinary.com/gokoura/image/upload/v1582937682/time_uqltwb.svg"
+        />
+        <Feature
+          title="Convenience"
+          desc="You never have to worry about carrying around or losing your locking
+            key again."
+          image="https://res.cloudinary.com/gokoura/image/upload/v1582938129/key_pkmkfx.svg"
+        />
+      </Stack>
     </Box>
   );
 };
