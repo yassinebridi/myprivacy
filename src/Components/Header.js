4f60100879f1fbc8f08340b72eb44f8ad76e9a54
Yassine Bridi <yassinebridii@gmail.com>
2020-03-07 17:08:08 +0100

    Responsive Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,38 @@
 import React from "react";
 import { Link } from "gatsby";
 import { Box, Heading, Flex, Text, Button } from "@chakra-ui/core";
+import { MdClose, MdMenu } from "react-icons/md";
 import urlSlug from "url-slug";
 import styled from "styled-components";
 
+const Overlay = styled(Box)`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(5px);
+`;
+
 const StyledLink = styled(Link)`
   display: block;
-  padding: 0 20px;
   font-weight: 600;
   &:hover {
     color: #e53e3e;
   }
 `;
 
-const MenuItems = ({ children, slug }) => (
-  <StyledLink to={slug} mt={{ base: 4, md: 0 }}>
-    <Text>{children}</Text>
+const MenuItems = ({ children, slug, mobile }) => (
+  <StyledLink
+    to={slug}
+    mt={{ base: 4, md: 0 }}
+    style={{
+      padding: mobile ? "20px 20px" : "0 20px",
+      margin: mobile ? "14px 0" : null
+    }}
+  >
+    <Text fontSize={mobile ? "2xl" : null}>{children}</Text>
   </StyledLink>
 );
 
@@ -23,6 +40,7 @@ const Header = props => {
   const [show, setShow] = React.useState(false);
   const handleToggle = () => setShow(!show);
 
+  console.log(show);
   return (
     <Flex
       as="nav"
@@ -41,20 +59,20 @@ const Header = props => {
         </Link>
       </Flex>
 
-      <Box display={{ sm: "block", md: "none" }} onClick={handleToggle}>
-        <svg
-          fill="white"
-          width="12px"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>Menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-        </svg>
-      </Box>
+      <Box
+        as={show ? MdClose : MdMenu}
+        display={{ sm: "block", md: "none" }}
+        onClick={handleToggle}
+        size="28px"
+        color="gray.600"
+      ></Box>
 
       <Box
-        display={{ sm: show ? "block" : "none", md: "flex" }}
+        display={{
+          xs: "none",
+          sm: "none",
+          md: "flex"
+        }}
         width={{ sm: "full", md: "auto" }}
         alignItems="center"
         flexGrow={1}
@@ -65,6 +83,53 @@ const Header = props => {
         <MenuItems slug="/blog">Blog</MenuItems>
         <MenuItems slug="/contact">Contact us</MenuItems>
       </Box>
+      <Overlay
+        display={{
+          xs: show ? "block" : "none",
+          sm: show ? "block" : "none",
+          md: "none"
+        }}
+        zIndex={1}
+        px={8}
+        py={10}
+        alignItems="center"
+        flexGrow={1}
+        justifyContent="flex-end"
+      >
+        <Box mb={8}>
+          <MenuItems mobile slug="/">
+            Home
+          </MenuItems>
+          <MenuItems mobile slug="/about">
+            About
+          </MenuItems>
+          <MenuItems mobile slug="/blog">
+            Blog
+          </MenuItems>
+          <MenuItems mobile slug="/contact">
+            Contact us
+          </MenuItems>
+        </Box>
+        <Flex
+          align="center"
+          justifyContent="space-between"
+          padding="0 15px"
+          mt={8}
+        >
+          <Box
+            as={show ? MdClose : MdMenu}
+            display={{ sm: "block", md: "none" }}
+            onClick={handleToggle}
+            size="40px"
+            color="gray.600"
+          ></Box>
+          <Link to="/">
+            <Text fontSize="3xl" fontWeight="bold">
+              Soley
+            </Text>
+          </Link>
+        </Flex>
+      </Overlay>
     </Flex>
   );
 };
