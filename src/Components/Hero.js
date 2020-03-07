81019c603180f7d895f53db2b78a4b15b20c6108
Yassine Bridi <yassinebridii@gmail.com>
2020-03-07 17:21:36 +0100

    Resonsive Hero

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -16,22 +16,19 @@ const Hero = props => {
       <Box display={{ md: "flex" }} textAlign="center" pt={10}>
         <Box width="100%" py={10}>
           <Heading
-            size="2xl"
+            fontSize={["2xl", "3xl", "4xl", "5xl"]}
             as="h1"
             fontWeight="bold"
             fontFamily="Libre Baskerville"
           >
-            Meet smarter,
+            Meet smarter,<br></br>more secure safes locks.
           </Heading>
-          <Heading
-            size="2xl"
-            as="h1"
-            fontWeight="bold"
-            fontFamily="Libre Baskerville"
+          <Text
+            color="gray.600"
+            fontSize={["xs", "sm", "lg", "xl"]}
+            fontWeight="light"
+            py="10"
           >
-            more secure safes locks.
-          </Heading>
-          <Text color="gray.600" fontSize="xl" fontWeight="light" py="10">
             Let us maximize your business safety <br></br> with cuting edge
             Facial Recognition powered safe locks.
           </Text>
